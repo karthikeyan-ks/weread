8f8386995d179da0fcffe2aaa642c57ba6087b51
SUFIYANJT <jtfinu@gmail.com>
2025-01-16 13:47:29 +0530

    done login page

--- libra/lib/home_screen.dart
+++ libra/lib/home_screen.dart
@@ -20,7 +20,9 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Header Image
             ClipRRect(
+              borderRadius: BorderRadius.circular(0),
               child: Image.network(
                 'https://images.pexels.com/photos/1480335/pexels-photo-1480335.jpeg',
                 width: double.infinity,
@@ -28,6 +30,7 @@ class HomeScreen extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
+            // Search Bar
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
@@ -40,9 +43,166 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            // Add other sections like 'Books', 'Categories', etc.
+            // Books Section
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Books',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'View all',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  BookCard(imageUrl: 'https://example.com/book1.jpg', name: 'Book 1', author: 'Author 1'),
+                  BookCard(imageUrl: 'https://example.com/book2.jpg', name: 'Book 2', author: 'Author 2'),
+                  BookCard(imageUrl: 'https://example.com/book3.jpg', name: 'Book 3', author: 'Author 3'),
+                ],
+              ),
+            ),
+            // Categories Section
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'View all',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CategoryCard(name: 'Novel'),
+                CategoryCard(name: 'Biography'),
+                CategoryCard(name: 'Science'),
+              ],
+            ),
+            // Rented Section
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Rented',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'View all',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  BookCard(imageUrl: 'https://example.com/rented1.jpg', name: 'Rented Book 1', author: 'Author 4'),
+                  BookCard(imageUrl: 'https://example.com/rented2.jpg', name: 'Rented Book 2', author: 'Author 5'),
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BookCard extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final String author;
+
+  BookCard({required this.imageUrl, required this.name, required this.author});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8.0),
+      width: 150,
+      child: Column(
+        children: [
+          Image.network(
+            imageUrl,
+            height: 100,
+            width: 100,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 8),
+          Text(
+            name,
+            style: TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            author,
+            style: TextStyle(color: Colors.grey),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String name;
+
+  CategoryCard({required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Text(
+        name,
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       ),
     );
   }
