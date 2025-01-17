71b4a7aa62d06cc2893fed7815d4ac76c5b82f45
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-01-17 09:44:56 +0530

    change

--- backend/weread/api/views.py
+++ backend/weread/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class TestAPIView(APIView):
-    def post(self, request):
+    def get(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
         print("the username and password is :",username,password)
