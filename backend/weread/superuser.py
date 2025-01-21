c5eff0278fffd92e0bff3aacf40ebf83579c5155
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-01-21 18:39:17 +0530

    hosting test

--- backend/weread/superuser.py
+++ backend/weread/superuser.py
@@ -3,8 +3,8 @@
 from django.core.management import call_command
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 # Check if the superuser already exists to avoid duplicate entries
-if not User.objects.filter(username='admin').exists():
+if not CustomUser.objects.filter(username='admin').exists():
     call_command('createsuperuser', '--noinput', username='admin', email='admin@example.com', password='admin')
