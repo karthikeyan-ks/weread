4acff80f977479d3f3d2876bca017fa6e3aef9ef
karthikeyan-ks <karthikeyanks3673@gmail.com>
2025-01-21 19:09:36 +0530

    hosting test 2

--- backend/weread/execute.sh
+++ backend/weread/execute.sh
@@ -2,5 +2,5 @@ pip install --upgrade mysqlclient
 pip show mysqlclient
 python manage.py makemigrations
 python manage.py migrate
-python superuser.py
+DJANGO_SETTINGS_MODULE=weread.settings python superuser.py
 python manage.py runserver 0.0.0.0:$PORT
--- backend/weread/superuser.py
+++ backend/weread/superuser.py
@@ -1,10 +1,22 @@
-# create_superuser.py
-
-from django.core.management import call_command
+import os
+import django
 from django.contrib.auth import get_user_model
 
+# Set up Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weread.settings')
+django.setup()
+
+# Get the custom user model
 CustomUser = get_user_model()
 
-# Check if the superuser already exists to avoid duplicate entries
+# Create superuser if it doesn't exist
 if not CustomUser.objects.filter(username='admin').exists():
-    call_command('createsuperuser', '--noinput', username='admin', email='admin@example.com', password='admin')
+    CustomUser.objects.create_superuser(
+        username='admin',
+        email='admin@example.com',
+        password='adminpassword',
+        is_site_admin=True  # Assuming `is_site_admin` is a required field
+    )
+    print("Superuser created successfully.")
+else:
+    print("Superuser already exists.")
